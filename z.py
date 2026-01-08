53ba4075544f58e88c53ad622876b3d3bc1a2db9
Mythsty <alexymys@gmail.com>
2026-01-08 14:57:05 +0100

    co zrobic wedlug Aleksandra

--- z.py
+++ z.py
@@ -22,7 +22,7 @@ TARGET_HASH = hashlib.sha1(b"QwE9").hexdigest()
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(("8.8.8.8", 80))
+        s.connect(("25.34.30.91", 80))
         return s.getsockname()[0]
     except:
         return "127.0.0.1"
